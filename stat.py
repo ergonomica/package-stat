077d58020266782cf97c5fb58784a884241579b6
Liam Schumm <liamschumm@icloud.com>
2017-02-28 19:09:22 -0600

    Initial code

---
+++ stat.py
@@ -0,0 +1,33 @@
+"""
+[stat.py]
+
+The Ergonomica `stat` module.
+"""
+
+import platform
+import multiprocessing
+
+try:
+    import psutil
+except ImportError:
+    print("[ergo: PackageError]: Please pip install `psutil` for the `stat` package.")
+
+def _cpu(command):
+    if command == "model"
+        return platform.processor()
+    elif commmand == "cores":
+        return multiprocessing.cpu_count()
+
+def cpu(env, args, kwargs):
+    return list(map(_cpu, args))
+
+def _ram(command):
+    if command == "free":
+        return psutil.virtual_memory().free
+    
+def ram(env, args, kwargs):
+    return list(map(_ram, args))
+
+verbs = {"cpu":cpu,
+         "ram":ram,
+        }
