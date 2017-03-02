4fe90685df597f07eef175c63a886abac5ab8edd
Liam Schumm <liamschumm@icloud.com>
2017-03-01 21:50:56 -0600

    Make 'cpu' command use all of cpuinfo's information

--- status.py
+++ status.py
@@ -10,20 +10,22 @@ import multiprocessing
 try:
     import psutil
 except ImportError:
-    raise Exception("[ergo: PackageError]: Please pip install `psutil` for the `stat` package.")
+    raise Exception("[ergo: PackageError]: Please pip install `psutil` for the `status` package.")
+
+try:
+    import cpuinfo
+except ImportError:
+    raise Exception("[ergo: PackageError]: Please pip install `cpuinfo` for the `status` package.")
 
 def _cpu(command):
-    if command == "model":
-        return platform.processor()
-    elif commmand == "cores":
-        return multiprocessing.cpu_count()
+    return cpuinfo.get_cpu_info[command]
 
 def cpu(env, args, kwargs):
     return list(map(_cpu, args))
 
- def _ram(command):
-     if command == "free":
-         return psutil.virtual_memory().free
+def _ram(command):
+    if command == "free":
+        return psutil.virtual_memory().free
      
 def ram(env, args, kwargs):
      return list(map(_ram, args))
